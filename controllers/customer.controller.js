1912a3366131255f7fc1af7a7bf383e9bc2d0f3c
Carlos Lopez <carlos@nousproyect.com>
2021-06-16 22:04:49 -0500

    feature: add method putCustomers and PostCustomers

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -1,17 +1,114 @@
-const {getConnect} = require('../cnn');
+const { getConnect } = require("../cnn");
 
-const getCustomers = async (req,res) =>{
-    const connection = await getConnect();
-    const result  = await connection.execute(`select * from customer`);
-    res.json(result.rows)
-}
+const getCustomers = async (req, res) => {
+  const connection = await getConnect();
+  const result = await connection.execute(`select * from customer`);
+  res.json(result.rows);
+  await connection.close();
+};
 
-const insertCustomers = async (req,res) =>{
-    const connection = await getConnect();
-    const result  = await connection.execute(`select * from customer`);
-}
+const postCustomers = async (req, res) => {
+  const connection = await getConnect();
+  const {
+    customer_id,
+    store_id,
+    first_name,
+    last_name,
+    email,
+    address_id,
+    active,
+  } = req.query;
+  const sql = `
+    Insert into customer
+    (customer_id,store_id,first_name,last_name,email,address_id,active,create_date,last_update)
+   Values
+   (:customer_id,:store_id,:first_name,:last_name,:email,:address_id,:active,:create_date,:last_update)
+   `;
+  try {
+    await connection.execute(sql, {
+      customer_id,
+      store_id,
+      first_name,
+      last_name,
+      email,
+      address_id,
+      active,
+      create_date: new Date(),
+      last_update: new Date(),
+    });
 
+    res.json({
+      message: "Customoer ingresado correctamente",
+      obejct: {
+        customer_id,
+        store_id,
+        first_name,
+        last_name,
+        email,
+        address_id,
+        active,
+        create_date: new Date(),
+        last_update: new Date(),
+      },
+    });
+
+    await connection.close();
+  } catch (error) {
+    console.log(error);
+    res.json({
+      error,
+    });
+  }
+};
+
+const putCustomers = async (req, res) => {
+  const connection = await getConnect();
+  const {
+    customer_id,
+    store_id,
+    first_name,
+    last_name,
+    email,
+    address_id,
+    active,
+  } = req.query;
+  const sql = `UPDATE customer set store_id=:store_id,first_name=:first_name,last_name=:last_name,email=:email,address_id=:address_id,active=:active,last_update=:last_update where  customer_id=:customer_id`;
+  try {
+    await connection.execute(sql, {
+      customer_id,
+      store_id,
+      first_name,
+      last_name,
+      email,
+      address_id,
+      active,
+      last_update: new Date(),
+    });
+
+    res.json({
+      message: "Customer actualizado correctamente",
+      obejct: {
+        customer_id,
+        store_id,
+        first_name,
+        last_name,
+        email,
+        address_id,
+        active,
+        last_update: new Date(),
+      },
+    });
+    await connection.close();
+  } catch (error) {
+    console.log(error);
+    res.json({
+      error,
+    });
+  }
+};
 
 module.exports = {
-    getCustomers
-}
+  getCustomers,
+  postCustomers,
+  putCustomers,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const {Router} = require('express')
-const {getCustomers} = require('../controllers/customer.controller')
+const {getCustomers,postCustomers,putCustomers} = require('../controllers/customer.controller')
 const route = Router()
 
 route.get('/',(req,res) => {res.send('Welcome API-REST Inventario!!!!')})
 route.get('/customers',getCustomers)
+route.post('/customers',postCustomers)
+route.put('/customers',putCustomers)
 
 module.exports = route
