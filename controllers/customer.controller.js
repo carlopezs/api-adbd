2b5db09d36c09129e4236b8dd2a9719775912079
Carlos Lopez <carlos@nousproyect.com>
2021-06-17 22:59:55 -0500

    creacion de todo el crud

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -2,10 +2,24 @@ const { getConnect } = require("../cnn");
 
 const getCustomers = async (req, res) => {
   const connection = await getConnect();
-  const result = await connection.execute(`select * from customer`);
+  const result = await connection.execute(`select cu.customer_id,cu.address_id,cu.store_id,cu.first_name,cu.last_name,cu.email, ad.address addres_name,cu.active,cu.create_date,cu.last_update from customer cu,address ad where cu.address_id = ad.address_id`);
   res.json(result.rows);
   await connection.close();
 };
+
+const getAddress = async (req, res) => {
+  const connection = await getConnect();
+  const result = await connection.execute(`select address_id,address from address`);
+  res.json(result.rows);
+  await connection.close();
+}
+
+const getStore = async (req, res) => {
+  const connection = await getConnect();
+  const result = await connection.execute(`select store_id from store`);
+  res.json(result.rows);
+  await connection.close();
+}
 
 const postCustomers = async (req, res) => {
   const connection = await getConnect();
@@ -17,7 +31,8 @@ const postCustomers = async (req, res) => {
     email,
     address_id,
     active,
-  } = req.query;
+  } = req.body;
+
   const sql = `
     Insert into customer
     (customer_id,store_id,first_name,last_name,email,address_id,active,create_date,last_update)
@@ -71,7 +86,8 @@ const putCustomers = async (req, res) => {
     email,
     address_id,
     active,
-  } = req.query;
+  } = req.body;
+  console.log(req.body);
   const sql = `UPDATE customer set store_id=:store_id,first_name=:first_name,last_name=:last_name,email=:email,address_id=:address_id,active=:active,last_update=:last_update where  customer_id=:customer_id`;
   try {
     await connection.execute(sql, {
@@ -109,7 +125,7 @@ const putCustomers = async (req, res) => {
 
 const deleteCustomer = async (req, res) => {
     const connection = await getConnect();
-    const {customer_id} = req.query;
+    const customer_id = req.params.id_customer;
     const sql = `DELETE FROM customer WHERE customer_id = :customer_id`;
     try {
        const resp =  await connection.execute(sql, {customer_id},{autoCommit: true});
@@ -133,5 +149,7 @@ module.exports = {
   getCustomers,
   postCustomers,
   putCustomers,
-  deleteCustomer
+  deleteCustomer,
+  getAddress,
+  getStore
 }
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const {Router} = require('express')
-const {getCustomers,postCustomers,putCustomers,deleteCustomer} = require('../controllers/customer.controller')
+const {getCustomers,postCustomers,putCustomers,deleteCustomer,getAddress,getStore} = require('../controllers/customer.controller')
 const route = Router()
 
 route.get('/',(req,res) => {res.send('Welcome API-REST Inventario!!!!')})
 route.get('/customers',getCustomers)
+route.get('/address',getAddress)
+route.get('/store',getStore)
+
 route.post('/customers',postCustomers)
 route.put('/customers',putCustomers)
 
-route.delete('/customers',deleteCustomer)
+route.delete('/customers/:id_customer',deleteCustomer)
 
 module.exports = route
